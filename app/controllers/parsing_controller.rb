170a5b77ed3af6ee4fb238a623e266eaeab90d18
Fuda Ryusuke <RyusukeFuda@Fuda-no-MacBook-Pro.local>
2014-07-06 14:55:36 +0900

    fit to coding rules and fix bugs

--- app/controllers/parsing_controller.rb
+++ app/controllers/parsing_controller.rb
@@ -4,11 +4,8 @@ class ParsingController < ApplicationController
 
   def index
 
-  feed = Feed.new
-  @news_json = feed.parse.to_json
-
-
-
+    feed = Feed.new
+    @news_json = feed.parse.to_json
 
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,33 +4,35 @@ class Feed
 
   def parse
 
-
-    persons = %w(田中将大 黒木メイサ 亀山耕平 新垣 結衣)
+    persons = %w(田中将大 黒木メイサ 亀山耕平 新垣結衣)
     baseurl_googlenews = "https://news.google.com/news/feeds?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss&q="
 
     news_array_raw = []
     result_parse = {}
+    article_url = ''
 
-    persons.each {|name|
+    persons.each do|name|
       feedUrl = baseurl_googlenews + name
       feedUrl = URI.escape(feedUrl)
       feed = Feedjira::Feed.fetch_and_parse(feedUrl)
 
       entry = feed.entries.first
+      url_raw = entry.url
+      article_url = url_raw.match(/url=.*$/).to_s
+      article_url.slice!(0,4)
+
       news_array_raw << {
-          'title' => entry.title,
-          'article_url' => entry.url,
-          'date' => entry.published
+          title: entry.title,
+          article_url: article_url,
+          date: entry.published
       }
-    }
+    end
 
     result_parse = {
       'result' => true,
       'status' => 200,
     }
     result_parse['body'] = news_array_raw
-
-    Rails.logger.debug(result_parse)
     result_parse
   end
 
