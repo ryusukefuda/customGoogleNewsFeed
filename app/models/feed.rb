40613932a8cbff0f3a49471329cebd052dfdb59d
Fuda Ryusuke <RyusukeFuda@Fuda-no-MacBook-Pro.local>
2014-07-08 05:41:04 +0900

    added peoples to array

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,14 +4,47 @@ class Feed
 
   def parse
 
-    persons = %w(田中将大 黒木メイサ 亀山耕平 新垣結衣)
+    personsEntertainment = %w(
+                          黒木メイサ
+                          新垣結衣
+                          瀬戸康史
+                          三浦翔平
+                          MayJ
+                          加藤ミリヤ
+                          濱田岳
+                          中尾明慶
+                          吉高由里子
+                          戸田恵梨香
+                          徳島えりか
+                          福田彩乃
+                          松坂桃李
+                          豊田エリー
+                          清水翔太
+                          西野カナ
+    )
+    personsSports = %w(
+                    田中将大
+                    坂本勇人
+                    吉川光夫
+                    香川真司
+                    吉田麻也
+                    乾貴士
+                    内村航平
+                    福原愛
+                    井岡一翔
+                    亀山耕平
+    )
     baseurl_googlenews = "https://news.google.com/news/feeds?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss&q="
 
     news_array_raw = []
     result_parse = {}
     article_url = ''
+    persons = []
 
-    persons.each do|name|
+    persons += personsEntertainment
+    persons += personsSports
+
+    persons.each do |name|
       feedUrl = baseurl_googlenews + name
       feedUrl = URI.escape(feedUrl)
       feed = Feedjira::Feed.fetch_and_parse(feedUrl)
