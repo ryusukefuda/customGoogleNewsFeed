387079e238ee7b2d6412d58c3e6f53b8262c7910
Fuda Ryusuke <ryukai_f@yahoo.co.jp>
2014-07-25 12:30:04 +0900

    modify syntax

--- app/controllers/parsing_controller.rb
+++ app/controllers/parsing_controller.rb
@@ -7,7 +7,7 @@ class ParsingController < ApplicationController
     feed = Feed.new
 
     start = params[:start].to_i
-    count = params[:count].to_i.blank? ? 10 : params[:count].to_i
+    count = params[:count].blank? ? 10 : params[:count].to_i
 
     @news_json = feed.parse(start, count).to_json
 
